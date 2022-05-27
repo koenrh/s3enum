7d805ca1f0e9f914195cf2c19bc96883a33fcd51
Koen Rouwhorst <info@koenrouwhorst.nl>
2022-05-27 18:12:03 +0200

    Check if bucket name target is regional/non-global
    S3 buckets located in regions out of us-east-1 have a different DNS
    response when addressed with the global S3 DNS suffix
    's3.amazonaws.com'. This allows us to determine whether an S3 bucket
    exists or not for all buckets except buckets located in the us-east-1
    region.

--- bucket_checker.go
+++ bucket_checker.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/miekg/dns"
 )
@@ -14,15 +16,15 @@ type Resolver interface {
 }
 
 // NewS3Resolver initializes a new S3Resolver
-func NewS3Resolver(ns string) (*S3Resolver, error) {
-	config, err := getConfig(ns)
+func NewS3Resolver(nsAddr string) (*S3Resolver, error) {
+	config, err := getConfig(nsAddr)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return &S3Resolver{
-		dnsClient: dns.Client{},
+		dnsClient: dns.Client{ReadTimeout: 3 * time.Second},
 		config:    *config,
 	}, nil
 }
@@ -32,30 +34,43 @@ type S3Resolver struct {
 	config    dns.ClientConfig
 }
 
-const s3host = "s3.amazonaws.com"
+const (
+	defaultPort    int    = 53
+	s3GlobalSuffix string = "s3.amazonaws.com."
+	s31WSuffix     string = "s3-1-w.amazonaws.com."
+)
 
 // IsBucket determines whether this prefix is a valid S3 bucket name.
 func (s *S3Resolver) IsBucket(name string) bool {
-	result, err := s.resolveCNAME(fmt.Sprintf("%s.%s.", name, s3host))
+	records, err := s.resolveName(fmt.Sprintf("%s.%s", name, s3GlobalSuffix))
 
-	if err == nil && !strings.Contains(result, "s3-directional") {
-		return true
+	if err != nil {
+		// TODO: Handle error
+		return false
 	}
 
-	return false
+	if len(records) != 1 || records[0].Header().Rrtype != dns.TypeCNAME {
+		return false
+	}
+
+	cname := records[0].(*dns.CNAME)
+
+	// The assumption is that existing bucket names (under the 's3.amazonaws.com' suffix)
+	// resolve to either a regional S3 name (e.g. 's3-w.eu-central-1.amazonaws.com.') or
+	// 's3-2-w.amazonaws.com.' and 's3-3-w.amazonaws.com.'. Hence, we assume that any bucket
+	// that does not resolve 's3-1-w.amazonaws.com.' is an existing bucket.
+	return cname.Target != s31WSuffix
 }
 
+//func getConfig(nameserver string, port string) (*dns.ClientConfig, error) {
 func getConfig(nameserver string) (*dns.ClientConfig, error) {
 	if nameserver != "" {
-		addr := net.ParseIP(nameserver)
-		if addr != nil {
-			return &dns.ClientConfig{
-				Servers: []string{addr.String()},
-				Port:    "53",
-			}, nil
-		}
+		h, p := parseHostAndPort(nameserver)
 
-		return nil, errors.New("invalid ip addr")
+		return &dns.ClientConfig{
+			Servers: []string{h},
+			Port:    strconv.Itoa(p),
+		}, nil
 	}
 
 	config, err := dns.ClientConfigFromFile("/etc/resolv.conf")
@@ -67,7 +82,24 @@ func getConfig(nameserver string) (*dns.ClientConfig, error) {
 	return config, nil
 }
 
-func (s *S3Resolver) resolveCNAME(name string) (string, error) {
+func parseHostAndPort(addr string) (host string, port int) {
+	if p := strings.Split(addr, ":"); len(p) == 2 {
+		host = p[0]
+		var err error
+		if port, err = strconv.Atoi(p[1]); err != nil {
+			port = defaultPort
+		}
+	} else {
+		host = addr
+	}
+
+	if port == 0 {
+		port = defaultPort
+	}
+	return
+}
+
+func (s *S3Resolver) resolveName(name string) ([]dns.RR, error) {
 	msg := dns.Msg{}
 	msg.SetQuestion(name, dns.TypeCNAME)
 
@@ -75,16 +107,14 @@ func (s *S3Resolver) resolveCNAME(name string) (string, error) {
 	r, _, err := s.dnsClient.Exchange(&msg, addr)
 
 	if err != nil {
-		return "", errors.New("probably a timeout")
+		return []dns.RR{}, errors.New("read timeout")
 	}
 
 	var answer = r.Answer
 
 	if len(answer) == 0 {
-		return "", errors.New("no resp")
+		return []dns.RR{}, errors.New("empty answer")
 	}
 
-	var answ = answer[0].(*dns.CNAME).Target
-
-	return answ, err
+	return answer, nil
 }
